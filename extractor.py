50eed943a2d7c8c8b0e0cda013fa1425eadecd2f
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2026-04-30 17:57:57 -0400

    created a method where it extracts text from a pdf

---
+++ extractor.py
@@ -0,0 +1,13 @@
+import pdfplumber
+
+
+
+def extract_from_pdf(file_path):
+    """Extract text from PDF file."""
+    text = ""
+    with pdfplumber.open(file_path) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
+    return text.strip()
